65de439d5d08a39d78b7e73502f1ee7022dcfd33
kpananjady <51126772+kpananjady@users.noreply.github.com>
2022-02-23 14:34:04 -0500

    changed config

--- src/modules/templated/client.tsx
+++ src/modules/templated/client.tsx
@@ -1,11 +1,7 @@
 // This is a automatically generated partial. Do not edit. 
 
 import ad from './Ad/client'; 
-import audio from './Audio/client'; 
-import newsletter from './Newsletter/client'; 
 
 export default { init: () => { 
   ad.init() 
-  audio.init() 
-  newsletter.init() 
 }};
