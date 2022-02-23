1792e7f3cd388c89e7a814641839fa27a595df69
kpananjady <51126772+kpananjady@users.noreply.github.com>
2022-02-23 13:05:36 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ArchieContent from "./modules/templated/ArchieContent";
+
+import data from "./data/data.json";
+import story from "./data/story.json";
+
+export default function App() {
+  return (
+    <div className='inno'>
+      <div className='inno-container'>
+        <div className='inno-content'>
+          <ArchieContent data={data.content} story={story} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/modules/templated/client.tsx
@@ -0,0 +1,11 @@
+// This is a automatically generated partial. Do not edit. 
+
+import ad from './Ad/client'; 
+import audio from './Audio/client'; 
+import newsletter from './Newsletter/client'; 
+
+export default { init: () => { 
+  ad.init() 
+  audio.init() 
+  newsletter.init() 
+}};
