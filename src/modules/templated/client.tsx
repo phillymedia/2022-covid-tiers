3b3312a349b0e983c90acfefe31dd38e240b5f17
kpananjady <51126772+kpananjady@users.noreply.github.com>
2022-03-01 16:11:56 -0500

    changed 0.5 to 50

--- src/modules/templated/client.tsx
+++ src/modules/templated/client.tsx
@@ -1,7 +1,9 @@
 // This is a automatically generated partial. Do not edit. 
 
 import ad from './Ad/client'; 
+import subscribe from './Subscribe/client'; 
 
 export default { init: () => { 
   ad.init() 
+  subscribe.init() 
 }};
